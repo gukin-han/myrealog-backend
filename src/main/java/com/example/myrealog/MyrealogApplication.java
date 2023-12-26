cfa95b1996560f7eae728d0bca9a095ed4d6210f
Gukin Han <gukin.dev@gmail.com>
2023-12-26 21:15:19 +0900

    create spring boot project

---
+++ src/main/java/com/example/myrealog/MyrealogApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrealog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrealogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrealogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myrealog/MyrealogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrealog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrealogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
